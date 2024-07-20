122448cd415e74b69494dd7f41620683f791ed9d
ejames0201 <erronjames4@gmail.com>
2024-07-20 11:13:38 -0400

    week 9

--- src/main/java/com/franklin/course_management/Controllers/StudentController.java
+++ src/main/java/com/franklin/course_management/Controllers/StudentController.java
@@ -93,20 +93,25 @@ public class StudentController implements ErrorController {
 						earnedPoints += a.getPointsEarned(); 
 					}
 				}
-				percentage = (earnedPoints / totalPoints) * 100;
 				
-				if (percentage >= 90) {
-					grade = "A";
-				} else if (percentage >= 80 && percentage < 90) {
-					grade = "B";
-				} else if (percentage >= 70 && percentage < 80) {
-					grade = "C";
-				} else if (percentage >= 60 && percentage < 70) {
-					grade = "D";
-				} else if (percentage < 60) {
-					grade = "F";
+				if (earnedPoints > 0) {
+					percentage = (earnedPoints / totalPoints) * 100;
+				
+					if (percentage >= 90) {
+						grade = "A";
+					} else if (percentage >= 80 && percentage < 90) {
+						grade = "B";
+					} else if (percentage >= 70 && percentage < 80) {
+						grade = "C";
+					} else if (percentage >= 60 && percentage < 70) {
+						grade = "D";
+					} else if (percentage < 60) {
+						grade = "F";
+					} else {
+						grade = "N/A";
+					}
 				} else {
-					grade = "N/A";
+					grade = "No graded assignments";
 				}
 			}
 			
